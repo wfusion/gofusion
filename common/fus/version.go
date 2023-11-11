00515b8df9792223ec5367adcc630cd407e3ae2d
windawings <windawings@gmail.com>
2023-11-12 00:07:38 +0800

    build(fus): upgrade framework from v1.1.0 to v1.1.3
    Change-Id: I2173b5b1af41b0496c510e50d3011873a136d32b

--- common/fus/version.go
+++ common/fus/version.go
@@ -2,6 +2,6 @@
 package main
 
 var (
-	gitCommit = "37dc26d87ed9f44a28976d7161e31605d2eb00bb"
-	gitTag    = "v1.1.0"
+	gitCommit = "ad290014da20f47bfa770234c5a2b8bad349bf5b"
+	gitTag    = "v1.1.3"
 )
