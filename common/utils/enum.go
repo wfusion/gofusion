befeaa0ad990e49c93d17a82ca414ccd5cffb4f1
windawings <windawings@gmail.com>
2025-06-02 11:26:00 +0800

    fix(utils): enum slice convert should use type assertion before type force convertion
    Change-Id: I3263154e77314f6069c1e44b461ac7b659172056

--- common/utils/enum.go
+++ common/utils/enum.go
@@ -51,7 +51,11 @@ type enumString[T comparable, TS ~[]T] struct {
 func (e *enumString[T, TS]) Enum(s string) TS {
 	s = e.caseSensitivityConv(s)
 	if v, ok := e.reversedMapping[s]; ok {
-		return TS(SliceConvert(v, e.elemSliceType))
+		vList, ok := SliceConvert(v, e.elemSliceType).([]T)
+		if !ok {
+			panic(errors.Errorf("enum string convert to slice failed [src%+v dst%+v]", v, e.elemSliceType))
+		}
+		return TS(vList)
 	}
 	return nil
 }
